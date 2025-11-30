11c6563d8df80bfdb3cccc0701e2ce007ec5168d
Sebastián Silva <ssilvamc@uc.cl>
2025-11-30 20:42:12 -0300

    feat: admin checks

--- src/routes/proposal.js
+++ src/routes/proposal.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const proposalController = require('../controllers/proposalController');
+const { requireAdmin } = require('../middleware/requireAdmin');
 
-router.post('/create', proposalController.createProposal);
-router.get('/', proposalController.getProposals);
+router.post('/create', requireAdmin, proposalController.createProposal);
+router.get('/', requireAdmin, proposalController.getProposals);
 
 module.exports = router;
