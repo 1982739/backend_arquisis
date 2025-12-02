aaafd5de8e406e4c3833fed7121b423b626ffaad
italo <141895683+iTraverso21@users.noreply.github.com>
2025-12-01 22:19:36 -0300

    Remove admin requirement for receiveWebhook route

--- src/routes/auctions.js
+++ src/routes/auctions.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const auctionController = require('../controllers/auctionController');
 const { requireAdmin } = require('../middleware/requireAdmin');
 
-router.post('/receive', requireAdmin, auctionController.receiveWebhook);
+router.post('/receive', auctionController.receiveWebhook);
 router.post('/create', requireAdmin, auctionController.createAuction);
 router.get('/', requireAdmin, auctionController.getAllAuctions);
+
 
 module.exports = router;
